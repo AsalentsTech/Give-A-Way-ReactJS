83ee02d12fbde2e4d8ae929779658d104b12c376
AsalentsTech <unknownasalent432@gmail.com>
2021-07-10 09:21:59 -0500

    deploying react app

---
+++ build/precache-manifest.c6b92dd995dbdf7cd5b17665cd3a71f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05d5e3db366557e0954d7c1dfa086a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0026f73e089218857c7",
+    "url": "/static/css/main.b79ca8a9.chunk.css"
+  },
+  {
+    "revision": "1560fe02ef6a5c1a242d",
+    "url": "/static/js/2.397be621.chunk.js"
+  },
+  {
+    "revision": "2141f7c0b43c4dbda66b817f44710e8d",
+    "url": "/static/js/2.397be621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0026f73e089218857c7",
+    "url": "/static/js/main.10dc2fbd.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
